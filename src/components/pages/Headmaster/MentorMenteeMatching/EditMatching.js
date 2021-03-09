c88257e8f05f2c5d04a46d1a541ff65148096f86
masantea <mercyansong@gmail.com>
2021-03-08 21:10:17 -0500

    Edit form without axios

--- src/components/pages/Headmaster/MentorMenteeMatching/EditMatching.js
+++ src/components/pages/Headmaster/MentorMenteeMatching/EditMatching.js
@@ -1,20 +1,41 @@
 import { connect } from 'react-redux';
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import { Form, Button, Select, DatePicker, TimePicker } from 'antd';
-
-const initialState = {
-  date: '',
-  type: 'success',
-  content: '',
-};
-
-function onChange(time, timeString) {
-  console.log(time, timeString);
-}
+import {
+  Form,
+  Layout,
+  Button,
+  Select,
+  DatePicker,
+  TimePicker,
+  Modal,
+} from 'antd';
 
 const EditMatching = props => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [form, setForm] = useState({
+    date: '',
+    start: '',
+    end: '',
+    type: 'success',
+    content: '',
+  });
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  function onChange(time, timeString) {
+    console.log(time, timeString);
+  }
+
   const [componentSize, setComponentSize] = useState;
 
   const onFormLayoutChange = ({ size }) => {
@@ -23,54 +44,82 @@ const EditMatching = props => {
 
   return (
     <>
-      <Form
-        labelCol={{
-          span: 4,
-        }}
-        wrapperCol={{
-          span: 14,
-        }}
-        layout="horizontal"
-        initialValues={{
-          size: componentSize,
-        }}
-        onValuesChange={onFormLayoutChange}
-        size={componentSize}
-      >
-        <Form.Item label="Edit" name="size"></Form.Item>
+      <Button type="primary" onClick={showModal}>
+        Edit
+      </Button>
 
-        <Form.Item label="Mentor">
-          <Select>
-            <Select.Option value="demo">Demo</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item label="Mentee">
-          <Select>
-            <Select.Option value="demo">Demo</Select.Option>
-          </Select>
-        </Form.Item>
+      <Layout>
+        <Modal visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+          <Form
+            labelCol={{
+              span: 4,
+            }}
+            wrapperCol={{
+              span: 14,
+            }}
+            layout="horizontal"
+            initialValues={{
+              size: componentSize,
+            }}
+            onValuesChange={onFormLayoutChange}
+            size={componentSize}
+          >
+            <div style={{ width: '100%' }}>
+              <Form.Item label="Mentor">
+                <Select>
+                  <Select.Option value="demo">Rob</Select.Option>
+                  <Select.Option value="demo">Kasi</Select.Option>
+                  <Select.Option value="demo">Mark</Select.Option>
+                </Select>
+              </Form.Item>
+            </div>
 
-        <Form.Item label="Date">
-          <DatePicker />
-        </Form.Item>
-        <Form.Item label="Time">
-          <TimePicker
-            use12Hours
-            format="h:mm A"
-            onChange={onChange}
-            style={{ width: 140 }}
-          />
-          <TimePicker
-            use12Hours
-            format="h:mm A"
-            onChange={onChange}
-            style={{ width: 140 }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button>Save</Button>
-        </Form.Item>
-      </Form>
+            <div style={{ width: '100%' }}>
+              <Form.Item label="Mentee">
+                <Select>
+                  <Select.Option value="demo">Remon</Select.Option>
+                  <Select.Option value="demo">Jose</Select.Option>
+                  <Select.Option value="demo">Ethan</Select.Option>
+                  <Select.Option value="demo">Thierno</Select.Option>
+                </Select>
+              </Form.Item>
+            </div>
+
+            <div style={{ width: '100%' }}>
+              <Form.Item label="Date">
+                <DatePicker />
+              </Form.Item>
+            </div>
+
+            <div style={{ width: '100%' }}>
+              <Form.Item label="Time">
+                <TimePicker
+                  use12Hours
+                  format="h:mm A"
+                  onChange={onChange}
+                  style={{ width: 140 }}
+                />
+                <TimePicker
+                  use12Hours
+                  format="h:mm A"
+                  onChange={onChange}
+                  style={{ width: 140 }}
+                />
+              </Form.Item>
+            </div>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: '100%' }}
+              >
+                Save
+              </Button>
+            </Form.Item>
+          </Form>
+        </Modal>
+      </Layout>
     </>
   );
 };
