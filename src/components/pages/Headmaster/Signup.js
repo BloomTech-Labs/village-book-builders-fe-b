a38b0ccbc1d5bd73c03fd196c1da4be4101e8275
Mark Ambro <wowmarkambro@gmail.com>
2021-03-22 20:27:38 -0500

    connected modal with calendar and redux

--- src/components/pages/Headmaster/Signup.js
+++ src/components/pages/Headmaster/Signup.js
@@ -1,51 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { Button, Form, Layout, Modal, Popover, Select, Typography } from 'antd';
 import moment from 'moment';
-
-import {
-  Layout,
-  Modal,
-  Form,
-  Input,
-  DatePicker,
-  TimePicker,
-  InputNumber,
-  Button,
-  Select,
-  Space,
-} from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as CA from '../../../state/actions';
+import { axiosWithAuth } from '../../../utils/axiosWithAuth';
 
 const { Option } = Select;
-const format = 'HH:mm';
 
-const Signup = () => {
+const Signup = ({ CalendarRef, toggleAddModal, showAddModal }) => {
   const [library, setLibrary] = useState([]);
   const [village, setVillage] = useState([]);
   const [mentor, setMentor] = useState([]);
   const [mentee, setMentee] = useState([]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const dispatch = useDispatch();
+  const eventDetails = useSelector(
+    state => state.CalReducer.selectedEventDetails
+  );
+  const computerID = useSelector(state => state.CalReducer.computerId);
   const [forms, setForms] = useState({
-    title: '',
+    title: 'Mentor: ..., Mentee: ...',
     start: '',
     end: '',
     mentee: '',
     mentor: '',
-    time_zone: '',
     methods: '',
-    computer: '',
     topic: '',
     village: '',
     library: '',
   });
-  // mode*********<
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
+  //!! mode*********<
+  // const showModal = () => {
+  //   toggleAddModal();
+  // };
   const handleOk = () => {
-    setIsModalVisible(false);
+    toggleAddModal();
   };
   const handleCancel = () => {
-    setIsModalVisible(false);
+    toggleAddModal();
   };
   // mode*********>
 
@@ -53,78 +44,105 @@ const Signup = () => {
     getMentordetai();
   }, []);
 
-  console.log(mentor, 'this is me');
   const getMentordetai = () => {
     //  setLoading(true)
-    axios.get('http://localhost:5000/mentor/').then(res => {
-      setMentor(res.data);
-    });
+    axiosWithAuth()
+      .get('/mentors')
+      .then(res => {
+        setMentor(res.data);
+      });
   };
 
   useEffect(() => {
     getMenteedetai();
   }, []);
 
-  console.log(mentor, 'this is me');
   const getMenteedetai = () => {
     //  setLoading(true)
-    axios.get('http://localhost:5000/mentee/').then(res => {
-      setMentee(res.data);
-    });
+    axiosWithAuth()
+      .get('/mentees')
+      .then(res => {
+        setMentee(res.data);
+      });
   };
 
   useEffect(() => {
     getvillages();
   }, []);
 
-  console.log(mentor, 'this is me');
   const getvillages = () => {
     //  setLoading(true)
-    axios.get('http://localhost:5000/village/').then(res => {
-      setVillage(res.data);
-    });
+    axiosWithAuth()
+      .get('/villages')
+      .then(res => {
+        setVillage(res.data);
+      });
   };
 
   useEffect(() => {
     getlibrary();
   }, []);
 
-  console.log(mentor, 'this is me');
   const getlibrary = () => {
     //  setLoading(true)
-    axios.get('http://localhost:5000/library/').then(res => {
-      setLibrary(res.data);
-    });
+    axiosWithAuth()
+      .get('/libraries')
+      .then(res => {
+        setLibrary(res.data);
+      });
   };
 
   const submitHandler = e => {
     e.preventDefault();
+
+    const tempForm = {
+      ...forms,
+      computerId: computerID,
+      start: eventDetails.startStr,
+      end: eventDetails.endStr,
+      title: `Mentor: ${forms.mentor} \n Mentee: ${forms.mentee}`,
+    };
     console.log('form submitted!');
 
-    axios
-      .post('http://localhost:5000/match', forms)
-      .then(response => console.log(response))
-      .catch(err => console.log(err));
-    setForms({
-      title: '',
-      start: '',
-      end: '',
-      mentee: '',
-      mentor: '',
-      time_zone: '',
-      methods: '',
-      computer: '',
-      topic: '',
-      village: '',
-      library: '',
-    });
+    axiosWithAuth()
+      .post(`/sessions`, tempForm)
+      .then(response => {
+        const calAPI = CalendarRef.current._calendarApi;
+        calAPI.addEvent(
+          {
+            title: tempForm.title,
+            start: eventDetails.startStr,
+            end: tempForm.end,
+            start_actual: eventDetails.startStr,
+            end_actual: tempForm.end,
+            allDay: false,
+            mentor: tempForm.mentor,
+            mentee: tempForm.mentee,
+          },
+          true // temporary event, replaced by redux state
+        );
+        // console.log('CAL REFF', CalendarRef.current);
+      })
+      .catch(err => console.log(err))
+      .finally(() => {
+        setForms({
+          title: ``,
+          start: '',
+          end: '',
+          mentee: '',
+          mentor: '',
+          methods: '',
+          topic: '',
+          village: '',
+          library: '',
+        });
+
+        dispatch(CA.clearChosenEventDetails());
+
+        handleOk();
+      });
   };
   // **********onchanges Start**********
-  const myOnChange = e => {
-    console.log(forms);
-    setForms({ ...forms, [e.target.name]: e.target.value });
-  };
-
   const handleDropdownMethods = value => {
     setForms({ ...forms, methods: value });
   };
@@ -133,10 +151,6 @@ const Signup = () => {
   };
   const handleDropdownVillage = value => {
     setForms({ ...forms, village: value });
-  };
-
-  const handleDropdownComputer = value => {
-    setForms({ ...forms, computer: value });
   };
 
   const selectMentor = value => {
@@ -148,208 +162,170 @@ const Signup = () => {
     setForms({ ...forms, mentee: x });
   };
 
-  const timeStart = v => {
-    setForms({ ...forms, start: v });
-  };
-
-  const timeChange = date => {
-    setForms({ ...forms, end: date });
-  };
-
-  const selectVillage = v => {
-    setForms({ ...forms, village: v });
-  };
   const selectLibrary = v => {
     setForms({ ...forms, library: v });
   };
 
   // *********** onchanges end************
-
-  const computer = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
   return (
     <>
-      <Button type="primary" onClick={showModal}>
-        Assign Student
-      </Button>
-      <Layout>
-        <Modal visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-          {/* ********************** */}
-          <h2
+      <Modal
+        visible={showAddModal}
+        okButtonProps={{ style: { display: 'none' } }}
+        onOk={handleCancel}
+        onCancel={handleCancel}
+      >
+        {/* ********************** */}
+        <h2
+          style={{
+            width: '90%',
+            marginLeft: '35%',
+            color: '#FF914D',
+            alignitems: 'center',
+          }}
+        >
+          {' '}
+          Assign Student
+        </h2>
+
+        <Form
+          name="nest-messages"
+          style={{
+            background: '#FFDCC3',
+            border: '#FFDCC3 solid 2px',
+            padding: '10px',
+            borderRadius: '10px',
+          }}
+        >
+          <div style={{ width: '90%', marginLeft: '5%' }}>
+            <Form.Item label="Title">
+              <Typography.Text>{forms.title}</Typography.Text>
+            </Form.Item>
+          </div>
+
+          <div style={{ width: '90%', marginLeft: '5%' }}>
+            <Form.Item label="Mentor">
+              <Select name="mentor" onChange={selectMentor}>
+                {mentor.map(m => (
+                  <Option key={m.id} value={m.first_name}>
+                    <Popover
+                      content={'teststt'}
+                      title="mentee"
+                      trigger="hover"
+                      placement="left"
+                    >
+                      {m.first_name} {m.last_name}
+                    </Popover>
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </div>
+
+          <div style={{ width: '90%', marginLeft: '5%' }}>
+            <Form.Item label="Mentee">
+              <Select name="mentee" onChange={selectMentee}>
+                {mentee.map(s => (
+                  <Option key={s.id} value={s.first_name}>
+                    {s.first_name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </div>
+
+          <div
             style={{
-              width: '90%',
-              marginLeft: '35%',
-              color: '#FF914D',
-              alignitems: 'center',
+              padding: '5px',
+              margin: '10px',
+              marginLeft: '5%',
+              display: 'flex',
+              flexWrap: ' wrap',
             }}
           >
-            {' '}
-            Assign Student
-          </h2>
+            <Form.Item label="Start">
+              <Typography.Text>
+                {moment(eventDetails.start).format('MM-DD-yyyy HH:MM')}
+              </Typography.Text>
+            </Form.Item>
 
-          <Form
-            name="nest-messages"
-            style={{
-              background: '#FFDCC3',
-              border: '#FFDCC3 solid 2px',
-              padding: '10px',
-              borderRadius: '10px',
-            }}
-          >
-            <div style={{ width: '90%', marginLeft: '5%' }}>
-              <Form.Item label="Title">
-                <Input
-                  name={'title'}
-                  value={forms.title}
-                  onChange={myOnChange}
-                />
+            <div style={{ marginLeft: '5%' }}>
+              <Form.Item label="End">
+                <Typography.Text>
+                  {moment(eventDetails.end)
+                    .add({ minute: 30 })
+                    .format('MM-DD-yyyy HH:MM')}
+                </Typography.Text>
               </Form.Item>
             </div>
+          </div>
 
-            <div style={{ width: '90%', marginLeft: '5%' }}>
-              <Form.Item label="Mentor">
-                <Select name="mentor" onChange={selectMentor}>
-                  {mentor.map(m => (
-                    <Option key={m.id} value={m.first_name}>
-                      {m.first_name}
-                    </Option>
-                  ))}
-                </Select>
-              </Form.Item>
-            </div>
+          <div style={{ width: '90%', marginLeft: '5%' }}>
+            <Form.Item label="Topic">
+              <Select
+                name="topic"
+                onChange={handleDropdownTopic}
+                placeholder="--choose topic--"
+              >
+                <Option value="math">Math</Option>
+                <Option value="science">Science</Option>
+                <Option value="history">History</Option>
+                <Option value="english">English</Option>
+              </Select>
+            </Form.Item>
+          </div>
 
-            <div style={{ width: '90%', marginLeft: '5%' }}>
-              <Form.Item label="Mentee">
-                <Select name="mentee" onChange={selectMentee}>
-                  {mentee.map(s => (
-                    <Option key={s.id} value={s.first_name}>
-                      {s.first_name}
-                    </Option>
-                  ))}
-                </Select>
-              </Form.Item>
-            </div>
+          <div style={{ width: '90%', marginLeft: '5%' }}>
+            <Form.Item label="Village">
+              <Select
+                name="village"
+                onChange={handleDropdownVillage}
+                placeholder="--choose village--"
+              >
+                {village.map(v => (
+                  <Option key={v.id} value={v.name}>
+                    {v.name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </div>
 
-            <div style={{ width: '90%', marginLeft: '5%' }}>
-              <Form.Item label="Time Zone">
-                <Input
-                  name={['time_zone']}
-                  value={forms.time_zone}
-                  onChange={myOnChange}
-                />
-              </Form.Item>
-            </div>
+          <div style={{ width: '90%', marginLeft: '5%' }}>
+            <Form.Item label="Library">
+              <Select
+                name="library"
+                onChange={selectLibrary}
+                placeholder="--choose library--"
+              >
+                {library.map(l => (
+                  <Option key={l.id} value={l.name}>
+                    {l.name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </div>
 
-            <div
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              onClick={submitHandler}
               style={{
-                padding: '5px',
-                margin: '10px',
+                background: '#FF914D',
+                color: 'white',
+                width: '90%',
                 marginLeft: '5%',
-                display: 'flex',
-                flexWrap: ' wrap',
+                border: 'white solid 2px',
               }}
             >
-              <Form.Item label="Start">
-                <TimePicker
-                  defaultValue={moment('12:08', format)}
-                  name={'start'}
-                  value={forms.start}
-                  onChange={timeStart}
-                />
-              </Form.Item>
-
-              <div style={{ marginLeft: '5%' }}>
-                <Form.Item label="End">
-                  <TimePicker
-                    defaultValue={moment('12:08', format)}
-                    name={'end'}
-                    value={forms.end}
-                    onChange={timeChange}
-                  />
-                </Form.Item>
-              </div>
-            </div>
-            <div style={{ width: '90%', marginLeft: '5%' }}>
-              <Form.Item label="Computer">
-                <Select
-                  name="computer"
-                  onChange={handleDropdownComputer}
-                  placeholder="--computer--"
-                >
-                  {computer.map(c => (
-                    <Option value={c}>{c}</Option>
-                  ))}
-                </Select>
-              </Form.Item>
-            </div>
-
-            <div style={{ width: '90%', marginLeft: '5%' }}>
-              <Form.Item label="Topic">
-                <Select
-                  name="topic"
-                  onChange={handleDropdownTopic}
-                  placeholder="--choose topic--"
-                >
-                  <Option value="math">Math</Option>
-                  <Option value="science">Science</Option>
-                  <Option value="history">History</Option>
-                  <Option value="english">English</Option>
-                </Select>
-              </Form.Item>
-            </div>
-
-            <div style={{ width: '90%', marginLeft: '5%' }}>
-              <Form.Item label="Village">
-                <Select
-                  name="village"
-                  onChange={handleDropdownVillage}
-                  placeholder="--choose village--"
-                >
-                  {village.map(v => (
-                    <Option key={v.id} value={v.name}>
-                      {v.name}
-                    </Option>
-                  ))}
-                </Select>
-              </Form.Item>
-            </div>
-
-            <div style={{ width: '90%', marginLeft: '5%' }}>
-              <Form.Item label="Library">
-                <Select
-                  name="library"
-                  onChange={selectLibrary}
-                  placeholder="--choose library--"
-                >
-                  {library.map(l => (
-                    <Option key={l.id} value={l.name}>
-                      {l.name}
-                    </Option>
-                  ))}
-                </Select>
-              </Form.Item>
-            </div>
-
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                onClick={submitHandler}
-                style={{
-                  background: '#FF914D',
-                  color: 'white',
-                  width: '90%',
-                  marginLeft: '5%',
-                  border: 'white solid 2px',
-                }}
-              >
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-          {/* ********************** */}
-        </Modal>
-      </Layout>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        {/* ********************** */}
+      </Modal>
     </>
   );
 };
