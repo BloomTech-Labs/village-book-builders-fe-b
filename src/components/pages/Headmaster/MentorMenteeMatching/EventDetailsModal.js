85b65dbe8ed255cc8aa4da8beeebb6c82c4956a5
Remon Abdou <remoo1901@hotmail.com>
2021-03-23 20:49:09 -0400

    eventcard

--- src/components/pages/Headmaster/MentorMenteeMatching/EventDetailsModal.js
+++ src/components/pages/Headmaster/MentorMenteeMatching/EventDetailsModal.js
@@ -49,7 +49,71 @@ const EventDetailsModal = ({
       ]}
     >
       {/* THIS WILL BE REPLACED WITH A BETTER UI */}
-      <p>{JSON.stringify(eventDetails, null, 2)}</p>
+      {/* <p>{JSON.stringify(eventDetails, null, 2)}</p>*/}
+      {/* <div><ldi>Title :</ldi>  <h3 class="box"> {eventDetails.title}</h3></div> 
+      <h3 class="box">start : {eventDetails.start}</h3>
+      <h3 class="box">end : {eventDetails.end}</h3>
+      <h3 class="box">Mentor : {eventDetails.mentorId}</h3>
+      <h3 class="box">Mentee : {eventDetails.menteeId}</h3>
+      <h3 class="box">Topic : {eventDetails.topic}</h3>
+      <h3 class="box">Location : {eventDetails.locationId}</h3>
+      <h3 class="box">Village : {eventDetails.villageId}</h3>
+      <h3 class="box">Library : {eventDetails.libraryId}</h3>
+      <h3 class="box">Computer : {eventDetails.computerId}</h3>
+ 
+    */}
+      <div class="box">
+        <div id="parents">
+          <div id="box1">Title :</div>
+          <div id="box2"> {eventDetails.title}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">start :</div>
+          <div id="box2"> {eventDetails.start}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">end :</div>
+          <div id="box2"> {eventDetails.end}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">Mentor :</div>
+          <div id="box2"> {eventDetails.mentorId}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">Mentee :</div>
+          <div id="box2"> {eventDetails.menteeId}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">Topic :</div>
+          <div id="box2"> {eventDetails.topic}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">Location :</div>
+          <div id="box2"> {eventDetails.locationId}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">Village :</div>
+          <div id="box2"> {eventDetails.villageId}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">Library :</div>
+          <div id="box2"> {eventDetails.libraryId}</div>
+        </div>
+        <br></br>
+        <div id="parents">
+          <div id="box1">Computer :</div>
+          <div id="box2"> {eventDetails.computerId}</div>
+        </div>
+        <span></span>
+      </div>
     </Modal>
   );
 };
